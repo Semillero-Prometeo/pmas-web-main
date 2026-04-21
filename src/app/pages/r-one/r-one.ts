b5c1956e0d813d1a6017823033abe02e57fdd3e0
Santiago Arias <santiari05@hotmail.com>
2026-04-21 18:33:50 -0500

    chore: Update GATEWAY_URL to use localhost for local development

--- src/app/pages/r-one/r-one.ts
+++ src/app/pages/r-one/r-one.ts
@@ -8,8 +8,6 @@ import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../../core/services/auth.service';
 import { GATEWAY_URL } from '../../core/constants/gateway';
 
-// const GATEWAY_URL = 'http://192.168.1.115:3000';
-const GATEWAY_URL = 'http://localhost:3000';
 const SEQ_STORAGE_KEY = 'r1_sequences';
 
 export type Tab = 'movements' | 'sequences';
